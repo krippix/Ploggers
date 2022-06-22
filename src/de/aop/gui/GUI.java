5edd22e1e2cb6be2bd32160c805dc88ee06e61a7
krippix <krippix@users.noreply.github.com>
2022-06-22 15:24:48 +0200

    exitable Window :poggies:

--- src/de/aop/gui/GUI.java
+++ src/de/aop/gui/GUI.java
@@ -1,13 +1,48 @@
 package de.aop.gui;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class GUI extends JFrame
 {
 	JFrame window;
+	JPanel basePanel;
+	JButton buttonGenerate;
 	
-	GUI()
+	public GUI()
 	{
 		this.window = new JFrame();
+		
+		// react to window beeing closed
+		this.window.addWindowListener
+		(
+			new WindowAdapter()
+			{
+				@Override
+				public void windowClosing(WindowEvent e)
+				{
+					System.out.println("Exiting Software!");
+					System.exit(0);
+				}		
+			}
+		);
+		
+		
+		this.basePanel = new JPanel();
+		this.window.add(this.basePanel);
+		
+		this.buttonGenerate = new JButton("Generate");
+		this.buttonGenerate.addActionListener(e->generatePlot());
+		
+		
+		
+		this.window.setVisible(true);
+	}
+	
+	
+	private void generatePlot()
+	{
+		System.out.println("Totally Printing out a plot now :D");
 	}
 }
--- src/de/aop/ploggers/Main.java
+++ src/de/aop/ploggers/Main.java
@@ -1,8 +1,11 @@
 package de.aop.ploggers;
+import de.aop.gui.*;
 
 public class Main {
 	public static void main(String args[])
 	{
 		System.out.println("Hello, World!");
+		
+		GUI gui = new GUI();
 	}
 }
