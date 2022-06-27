83a79e84730693c5bde89c9577c0e9a3ba8652dd
Lauchmelder <robert.altner11@gmail.com>
2022-06-27 19:41:22 +0200

    add docs to Function

--- src/de/aop/parser/Function.java
+++ src/de/aop/parser/Function.java
@@ -2,8 +2,13 @@ package de.aop.parser;
 
 import de.aop.exceptions.SyntaxError;
 
+/**
+ * A wrapper around a Parser object with some additional
+ * QoL life methods for function evaluation and analysis
+ */
 public class Function
 {
+	// Used in the numerical differentiation algorithm
 	static double MACHINE_EPSILON = Math.ulp(1.0);
 	
 	Parser parser = null;
@@ -11,9 +16,12 @@ public class Function
 	public Function(String input)
 	{
 		parser = new Parser(input);
-		System.out.println(MACHINE_EPSILON);
 	}
 	
+	/**
+	 * Whether the parsing was successful
+	 * @return True if the expression was parsed without errors
+	 */
 	public boolean isValid()
 	{
 		return parser != null && parser.good();
@@ -24,21 +32,37 @@ public class Function
 		return parser.getError();
 	}
 	
+	/**
+	 * Value of the function at position x
+	 * @param x Position to evaluate the function at
+	 * @return The value of the function at position x
+	 */
 	public double at(double x)
 	{
 		return parser.eval(x);
 	}
 	
-	public double at(double x, int derivative) throws IllegalArgumentException
+	/**
+	 * Value of the n-th derivative of the function, evaluated at x
+	 * @param x Position to evaluate f^(n) at
+	 * @param n Which derivative of the function to evaluate (must be non-negative)
+	 * @return The value of the n-th derivative of the function at position x
+	 * 
+	 * @throws IllegalArgumentException If n was negative
+	 */
+	public double at(double x, int n) throws IllegalArgumentException
 	{
-		if(derivative < 0)
+		if(n < 0)
 			throw new IllegalArgumentException("derivative has to be a non-negative integer");
 		
-		if(derivative == 0)
+		// 0th derivative is just the function itself
+		if(n == 0)
 			return parser.eval(x);
 		
+		// Calculate h such that x+h and x-h have exact representations as floating point numbers
 		double h = Math.cbrt(MACHINE_EPSILON) * x;
 		
-		return (this.at(x + h, derivative - 1) - this.at(x - h, derivative - 1)) / (2.0*h);
+		// Perform numerical differentiation
+		return (this.at(x + h, n - 1) - this.at(x - h, n - 1)) / (2.0*h);
 	}
 }
