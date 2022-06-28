b19d5662a2208679308ec3b0615f2060bc68ff0a
Lauchmelder <robert.altner11@gmail.com>
2022-06-28 17:46:02 +0200

    added pole finding

--- src/de/aop/parser/Function.java
+++ src/de/aop/parser/Function.java
@@ -19,9 +19,10 @@ public class Function
 	private Interval domain;
 	private Interval range;
 	
-	private ArrayList<Double> roots 		= new ArrayList<Double>();
-	private ArrayList<Double> extrema 		= new ArrayList<Double>();
-	private ArrayList<Double> inflections 	= new ArrayList<Double>();
+	private ArrayList<Double> roots;
+	private ArrayList<Double> extrema;
+	private ArrayList<Double> inflections;
+	private ArrayList<Double> poles;
 	
 	public Function(String input, double from, double to)
 	{
@@ -31,7 +32,12 @@ public class Function
 			return;
 		
 		domain = new Interval(from, to);
-		range = new Interval(this.at(from), this.at(from));
+		this.analyze();
+	}
+	
+	private void analyze()
+	{
+		range = new Interval(this.at(domain.min), this.at(domain.min));
 		
 		for(double x = domain.min; x <= domain.max; x += 0.01)
 		{
@@ -40,13 +46,29 @@ public class Function
 			range.max = Math.max(range.max, y);
 		}
 		
+		roots 		= new ArrayList<Double>();
+		extrema 	= new ArrayList<Double>();
+		inflections = new ArrayList<Double>();
+		poles 		= new ArrayList<Double>();
+		
 		findRoots(roots, domain, 0);
 		findRoots(extrema, domain, 1);
 		findRoots(inflections, domain, 2);
 		
+		findPoles(domain);
+		
 		System.out.println("Roots      :" + roots.toString());
 		System.out.println("Extrema    :" + extrema.toString());
 		System.out.println("Inflections:" + inflections.toString());
+		System.out.println("Poles      :" + poles.toString());
+	}
+	
+	public void setDomain(double newMin, double newMax)
+	{
+		this.domain.min = newMin;
+		this.domain.max = newMax;
+		
+		this.analyze();
 	}
 	
 	/**
@@ -122,7 +144,7 @@ public class Function
 		findRoots(dest, new Interval(searchInterval.max, interval.max), n);
 		
 		Interval originalSearchInterval = new Interval(searchInterval.min, searchInterval.max);
-		boolean foundRoot = true;
+		boolean foundRoot = false;
 		double rootVal = 0.0;
 		
 		for(int i = 0; i < MAX_ITERATIONS; i++)
@@ -137,6 +159,9 @@ public class Function
 			
 			if (searchInterval.max - searchInterval.min <= MAX_ERROR)
 			{
+				if(Math.abs(this.at(searchInterval.max, n) - this.at(searchInterval.min, n)) > MAX_ERROR)
+					continue;
+				
 				foundRoot = true;
 				rootVal = searchInterval.center();
 				break;
@@ -148,6 +173,62 @@ public class Function
 			dest.add(rootVal);
 			findRoots(dest, new Interval(originalSearchInterval.min, rootVal - 0.01), n);
 			findRoots(dest, new Interval(rootVal + 0.01, originalSearchInterval.max), n);
+		}
+	}
+	
+	private double inverseAt(double x)
+	{
+		double y = this.at(x);
+		if (Double.isNaN(y))
+			return 0.0;
+		
+		return 1.0 / y;
+	}
+	
+	private void findPoles(Interval interval)
+	{
+		final int MAX_ITERATIONS = 100;
+		final double MAX_ERROR = 0.000001;
+		
+		Interval searchInterval = new Interval(interval.min, interval.min);
+		while(searchInterval.max <= interval.max && Math.signum(this.inverseAt(searchInterval.min)) == Math.signum(this.inverseAt(searchInterval.max)))
+			searchInterval.max += 0.01;
+		
+		if(searchInterval.max >= interval.max || Math.signum(this.inverseAt(searchInterval.min)) == Math.signum(this.inverseAt(searchInterval.max)))
+			return;
+		
+		findPoles(new Interval(searchInterval.max, interval.max));
+		
+		Interval originalSearchInterval = new Interval(searchInterval.min, searchInterval.max);
+		boolean foundRoot = false;
+		double rootVal = 0.0;
+		
+		for(int i = 0; i < MAX_ITERATIONS; i++)
+		{
+			double c = this.inverseAt(searchInterval.center());
+			System.out.println(c);
+			
+			if(Math.signum(c) == Math.signum(this.inverseAt(searchInterval.min)))
+				searchInterval.min = searchInterval.center();
+			else
+				searchInterval.max = searchInterval.center();
+			
+			if (searchInterval.max - searchInterval.min <= MAX_ERROR)
+			{
+				if(Math.abs(this.inverseAt(searchInterval.max) - this.inverseAt(searchInterval.min)) > MAX_ERROR)
+					continue;
+				
+				foundRoot = true;
+				rootVal = searchInterval.center();
+				break;
+			}
+		}
+		
+		if (foundRoot)
+		{
+			poles.add(rootVal);
+			findPoles(new Interval(originalSearchInterval.min, rootVal - 0.01));
+			findPoles(new Interval(rootVal + 0.01, originalSearchInterval.max));
 		}
 	}
 }
